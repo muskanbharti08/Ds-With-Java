bfb9837712ce10d674f2faaab375e7fb2003b574
muskan <muskanbharti.cse2020@dscet.ac.in>
2023-07-26 14:47:23 +0530

    HellowWorldCode

---
+++ Stream/src/PrintHellowWorld.java
@@ -0,0 +1,10 @@
+//Ques: It is java Program to print Hellow World 5 times using for loop
+public class PrintHellowWorld {
+    public static void main(String[] args) {
+        for (int i = 1 ; i <=5 ; i++) {
+            System.out.println("Hellow World");
+
+        }
+    }
+
+}
--- Stream/src/jk.java
+++ Stream/src/jk.java
@@ -1,5 +1,5 @@
 public class jk {
     public static void main(String[] args) {
-        System.out.println("Good Afternoon"); 
+        System.out.println("Good Afternoon");
     }
 }
