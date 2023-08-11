026fe65bbeac1ceaeebdd85bc5b0601ca5ba12b2
muskan <muskanbharti.cse2020@dscet.ac.in>
2023-08-11 17:26:13 +0530

    ISCode

---
+++ Stream/src/InsertionSort.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int [] Array = {2,3,4,87,98,56}; //  the given unsorted Array
+
+        for (int i = 0; i < Array.length; i++) {
+            System.out.print(Array[i]+" ");     // for printing  the array
+
+        }
+
+    }
+
+    public static void Insertion(int [] ar){
+        int n = ar.length;                  // initilizing the length of an Array
+        for (int i = 1; i <n ; i++) {  // i=1 beacause no need to sort index 0
+            int temp = ar[i];
+            int j = i-1;    // for enter in sorted part
+            while(j >= 0  &&  temp < ar[j]){
+                ar[j+1] = ar[j];
+                j--;
+
+            }
+            ar[j+1] = temp;
+        }
+
+
+    }
+}
+
+
