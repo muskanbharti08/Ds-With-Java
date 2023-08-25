ecec13cf52a6dbe238c462180384b6033536e66f
muskan <muskanbharti.cse2020@dscet.ac.in>
2023-08-25 16:42:06 +0530

    TCode

---
+++ Stream/src/Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int Facorial = 10;
+        for (int i = 10; i >=1 ; i--) {
+            System.out.print(i +" * ");
+
+        }
+        
+    }
+}
