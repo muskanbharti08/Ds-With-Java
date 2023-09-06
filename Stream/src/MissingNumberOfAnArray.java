5cf2ed6e9e8fba6206498519439137f59137acae
muskan <muskanbharti.cse2020@dscet.ac.in>
2023-09-06 07:17:58 +0530

    MECode

---
+++ Stream/src/MissingNumberOfAnArray.java
@@ -0,0 +1,26 @@
+public class MissingNumberOfAnArray {
+    public static void main(String[] args) {
+     int [] GivenArray={2,4,5,1,6,};
+   int a =   MissingELement(GivenArray);
+        System.out.println("The Missing numbers are from the Array is :"+a);
+    }
+    public static void Print(int [] p){
+        for (int i = 0; i <p.length ; i++) {
+            System.out.println(p[i]);
+
+        }
+    }
+
+    public static int MissingELement(int [] Array){
+        int n = Array.length+1;
+        int Sum = n*(n+1)/2;
+
+        for (int i = 0; i < Array.length; i++) {
+            
+            Sum-=Array[i];
+
+        }
+        return Sum;
+    }
+
+}
